db1472c06ef68a6535531517670b43985ecd2438
João Henrique <joaohrm@gmail.com>
2021-11-29 21:51:42 -0300

    listagem das receitas com get e inserts de teste no arquivo create.sql

--- src/main/java/com/ProjetoFullStack/ControleDeGastos/model/entity/controller/ReceitaController.java
+++ src/main/java/com/ProjetoFullStack/ControleDeGastos/model/entity/controller/ReceitaController.java
@@ -1,4 +1,27 @@
 package com.ProjetoFullStack.ControleDeGastos.model.entity.controller;
 
+import com.ProjetoFullStack.ControleDeGastos.model.entity.entity.Receita;
+import com.ProjetoFullStack.ControleDeGastos.model.entity.repository.ReceitaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/receita")
 public class ReceitaController {
+
+    private final ReceitaRepository repository;
+
+    @Autowired
+    public ReceitaController(ReceitaRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Receita> listar(){
+        return repository.findAll();
+    }
 }
--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -6,3 +6,10 @@ CREATE DATABASE "ProjetoFullStack"
     LC_CTYPE = 'Portuguese_Brazil.1252'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
+
+
+--inserts para popular tabela de receita para testes
+
+insert into receita values(1, NOW(), NOW(), 'Despesa 1', 100.00);
+insert into receita values(2, NOW(), NOW(), 'Despesa 2', 932.15);
+insert into receita values(3, NOW(), NOW(), 'Despesa 3', 230.28);
