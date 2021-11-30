8349fadb466a42fde905462ef39df5107e1680e0
João Henrique <joaohrm@gmail.com>
2021-11-29 21:12:57 -0300

    adicionado arquivo de criação da database, alterado properties com mais opções de login

---
+++ src/main/resources/create.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE "ProjetoFullStack"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
